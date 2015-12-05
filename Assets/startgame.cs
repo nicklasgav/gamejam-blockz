a64ba15517e5d6d6a17688b16f8447ccf37ca595
HerrBucket <claes.henrik.olsson@gmail.com>
2015-12-05 17:21:19 +0100

    Toybox prefab

---
+++ Assets/startgame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class startgame : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField] private Button MyButton = null; // assign in the editor
+	
+	void Start() { 
+		MyButton.onClick.AddListener(() => { 
+			Application.LoadLevel("Level 1");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
